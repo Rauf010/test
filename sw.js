5189c56dbaca65efe50b08aca4b8e151139c6fbd
rauf <raufabdoellah@gmail.com>
2025-12-13 18:58:11 +0100

    Proto

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "shadowcrew-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
